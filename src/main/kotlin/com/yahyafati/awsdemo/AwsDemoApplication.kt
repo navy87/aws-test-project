63794c7bd12c9a21743620ebffac585b400024f8
Yahya Fati <yfati037@gmail.com>
2024-02-03 13:57:43 +0300

    initial commit

---
+++ src/main/kotlin/com/yahyafati/awsdemo/AwsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.yahyafati.awsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<AwsDemoApplication>(*args)
+}
